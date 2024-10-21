844bba129c4753f9c356c0b64ebf0e73696a8cb0
vrarnold@iastate.edu <glpat-yzf_y4S1QM8gPQ4Rgxc9>
2024-10-20 19:12:22 -0500

    Finished work on Main and Songs page

--- songs.js
+++ songs.js
@@ -1,3 +1,5 @@
+let filteredSongs = [];
+
 function showArtist(artist) {
     fetch("./data.json")
         .then(response => response.json())
@@ -6,51 +8,47 @@ function showArtist(artist) {
 }
 
 function loadSongs(mySongs, artist) {
-    // ---------------------
-    // make array of objects
-    // ---------------------
     const arraySongs = [];
 
     for (let i = 0; i < mySongs.songs.length; i++) {
         arraySongs.push(mySongs.songs[i]);
     }
 
-    console.log(arraySongs);
-
-    // ---------------------
-    // sort array
-    // ---------------------
-    let sortedSongs = [];
-
-    // Filter the array of songs by the given artist
-    console.log(artist);
-    sortedSongs = arraySongs.filter(song => song.artist === artist);
-
-    // If no songs match the artist, return a message
-    if (sortedSongs.length === 0) {
-        console.log(`No songs found for ${artist}`);
+    if (artist != 'All Artists') {
+        filteredSongs = arraySongs.filter(song => song.artist === artist);
+        if (filteredSongs.length === 0) {
+            console.log(`No songs found for ${artist}`);
+        }
+    } else {
+        filteredSongs = arraySongs;
     }
 
-    // Add Artist Text
+    // Default sort by streams
+    sortSongs(1);
+
+    // Display the artist's songs
+    displaySongs(filteredSongs, artist);
+}
+
+function displaySongs(songs, artist) {
+    // Update Artist Text
     var ArtistTitle = document.getElementById("artist-title");
     ArtistTitle.innerHTML = artist.toUpperCase();
 
-    // ---------------------
-    // Construct the CARD
-    // ---------------------
+
     var CardSongs = document.getElementById("col");
-    // Clear previous songs data
-    CardSongs.innerHTML = ""; // This will clear the previous songs data and image
+    CardSongs.innerHTML = ""; // Clear the previous song data
 
-    for (var i = 0; i < sortedSongs.length; i++) {
-        let name = sortedSongs[i].name;
-        let artist = sortedSongs[i].artist;
-        let album = sortedSongs[i].album;
-        let streams = sortedSongs[i].streams;
-        let date = sortedSongs[i].date;
-        let url = sortedSongs[i].url;
+    // Display songs in cards
+    for (var i = 0; i < songs.length; i++) {
+        let name = songs[i].name;
+        let artist = songs[i].artist;
+        let album = songs[i].album;
+        let streams = songs[i].streams;
+        let date = songs[i].date;
+        let url = songs[i].url;
+
         let AddCardSongs = document.createElement("div");
-
         AddCardSongs.classList.add("col"); // Add Bootstrap class to the column
         AddCardSongs.innerHTML = `
         <div class="card shadow-sm">
@@ -64,148 +62,23 @@ function loadSongs(mySongs, artist) {
         </div>
         `;
         CardSongs.appendChild(AddCardSongs);
-    } // end of for
-} // end of function appendData
-
-
-/* function appendData(data) {
-    var CardSong = document.getElementById("col");
-    const m = document.getElementById("songs");
-    const inputSongName = m.value
-    CardSong.innerHTML = ""; // This will clear the previous songs data and image
-    for (let i = 0; i < Object.keys(data.songs).length; i++) {
-        if (data.songs[i].name === inputSongName) {
-            let name = data.songs[i].name;
-            let desc = data.songs[i].desc;
-            let listeners = data.songs[i].listeners;
-            let url = data.songs[i].url;
-            console.log(name);
-            let AddCardSong = document.createElement("div");
-            AddCardSong.classList.add("col"); // Add Bootstrap class to the column
-            AddCardSong.innerHTML = `
-            <div class="card shadow-sm">
-                <img src=${url} class="card-img-top" alt="..."></img>
-                <div class="card-body">
-                    <p class="card-text"> <strong>${name}</strong>, ${desc},${listeners}</p>
-                </div>
-            </div>
-            `;
-            CardSong.appendChild(AddCardSong);
-        }
     }
 }
 
+function sortSongs(sortType) {
+    if (sortType === 1) {
+        // Sort by streams
+        filteredSongs.sort((a, b) => {
+            // Removes commas to make streams a number, idea from https://www.basedash.com/blog/how-to-remove-commas-from-a-string-in-javascript
+            const streamsA = parseInt(a.streams.replace(/,/g, ''), 10);
+            const streamsB = parseInt(b.streams.replace(/,/g, ''), 10);
+            return streamsB - streamsA;
+        });
+    } else if (sortType === 2) {
+        // Sort by date
+        filteredSongs.sort((a, b) => b.date - a.date);
+    }
 
-
-function fetchData() {
-    const b = document.getElementById("my_form");
-    b.addEventListener("submit", (event) => {
-        event.preventDefault(); // Prevent the form from submitting in the traditional way
-        fetch("./data.json")
-            .then((response) => { return response.json() })
-            .then((data) => { appendData(data) })
-            .catch((error) => { console.log("Error " + error) });
-        console.log("End fetch");
-    });
+    // Display songs
+    displaySongs(filteredSongs, document.getElementById("artist-title").innerHTML);
 }
-
-
-
-function showAllSongs() { // shows all song called when clicked in navbar
-    fetch("./data.json")
-        .then(response => response.json())
-        .then(mySongs => loadSongs(mySongs, 1, style))
-        .catch(err => console.log("Error :" + err));
-}
-
-// sorted by style, called when clicking in home
-function showSongsByStyle(style) {
-    fetch("./data.json")
-        .then(response => response.json())
-        .then(mySongs => loadSongs(mySongs, 2, style))
-        .catch(err => console.log("Error :" + err));
-}
-
-
-
-function loadSongs(mySongs, option, style) {
-
-    let arraySongs = [];
-    let sortedSongs = [];
-
-    for (let i = 0; i < mySongs.songs.length; i++) {
-        arraySongs.push(mySongs.songs[i])
-    }
-
-    if (option === 1) {
-        // Show all songs
-        sortedSongs = arraySongs;
-    }
-    // Option 2 is to sort by style if style is rap then show all rap songs
-    else if (option === 2) {
-        // Filter songs by style
-        for (let song of arraySongs) {
-            if (song.style === style) {
-                sortedSongs.push(song);
-            }
-        }
-    }
-
-    else if (option === 3) {  
-        const inputDescription = document.getElementById("descriptionInput").value;
-        document.getElementById('inputField').style.display = 'none';
-        for (let songs of arraySongs) {
-            if (songs.description.includes(inputDescription)) {
-                sortedSongs.push(songs);
-            }
-        }
-        console.log(sortedSongs);
-    }
-
-
-
-    var CardSongs = document.getElementById("col");
-    CardSongs.innerHTML = "";
-
-
-    for (let i = 0; i < sortedSongs.length; i++) {
-        let title = sortedSongs[i].name;
-        let listeners = sortedSongs[i].listeners;
-        let url = sortedSongs[i].url;
-
-        let description = sortedSongs[i].description
-        let AddCardSongs = document.createElement("div");
-        AddCardSongs.classList.add("col");
-        AddCardSongs.innerHTML = `
-            <div class="card shadow-sm">
-                <img src=${url} class="card-img-top" alt="..."></img>
-                <div class="card-body">
-                    <p class="card-text"><strong>${title}</strong>, ${listeners}, $${price}, ${description}</p>
-                </div>
-            </div>
-        `;
-        CardSongs.appendChild(AddCardSongs);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-fetchData(); */
